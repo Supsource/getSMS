74932702f1735c97e9322f4656710d0bceeea529
Supriyo <supriyomalstartup@gmail.com>
2024-09-29 14:30:34 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,23 +7,19 @@ import os
 app = Flask(__name__)
 CORS(app) 
 
-
 SHEET_URL = 'https://docs.google.com/spreadsheets/d/1UwJs99EHSAZl9d1YSmlWPABKrT6nzVU3QzJeirtkJwo/edit?gid=0#gid=0'
 CREDENTIALS_FILE = 'config/credentials.json'
-
 
 @app.route('/api/due-users', methods=['GET'])
 def get_due_users():
     users_due_soon = filter_due_users(SHEET_URL, CREDENTIALS_FILE)
     return jsonify(users_due_soon.to_dict(orient="records"))
 
-
 @app.route('/api/send-sms', methods=['POST'])
 def send_sms():
     users_due_soon = filter_due_users(SHEET_URL, CREDENTIALS_FILE)
     send_bulk_sms(users_due_soon)
     return jsonify({'message': 'SMS sent to all users due soon'})
-
 
 @app.route('/api/export-csv', methods=['GET', 'POST'])  
 def export_csv():
@@ -39,4 +35,4 @@ def export_csv():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
